6b7df20bc42d51338eb2749edd117d8131f09cb1
Nils Ryter <nils.ryter@he-arc.ch>
2016-03-27 17:04:26 +0200

    Add the implementation of connexion

---
+++ fierce-galaxy/FierceGalaxyServer/ConnexionModule/ConnexionService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FierceGalaxyServer.ConnexionModule
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ConnexionService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ConnexionService.svc or ConnexionService.svc.cs at the Solution Explorer and start debugging.
+    public class ConnexionService : IConnexionService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ fierce-galaxy/FierceGalaxyServer/ConnexionModule/PlayerManagement.cs
@@ -0,0 +1,22 @@
+﻿using FierceGalaxyInterface.ConnexionModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FierceGalaxyInterface.GameModule;
+
+namespace FierceGalaxyServer.ConnexionModule
+{
+    public class PlayerManagement : IPlayerManagement
+    {
+        public IPlayer CreatePlayer(string playerID, string playerPW, string publicPseudo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlayer Login(string playerID, string playerPW)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
