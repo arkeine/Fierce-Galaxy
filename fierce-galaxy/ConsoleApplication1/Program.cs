3e22598a5ed092c8309c550098eb93a017c1093b
matthieu.bandelie <matbandeli@gmail.com>
2016-03-30 17:08:06 +0200

    mateli servertimeTests

---
+++ fierce-galaxy/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApplication1.TimeModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var time = new NetworkTime().GetNetworkTime();
+            var myTime = DateTime.Now;
+            var diffTime = time - myTime;
+            
+            Console.WriteLine(time.ToString() + " / diff : " +diffTime.TotalMilliseconds.ToString());
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
---
+++ fierce-galaxy/fierce-galaxy-interface/TimeModule/INetworkTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FierceGalaxyInterface.TimeModule
+{
+    public interface INetworkTime
+    {
+        /// <summary>
+        /// Return the time from a default NTP server
+        /// </summary>
+        DateTime GetNetworkTime();
+
+        /// <summary>
+        /// Return the time from the given NTP server
+        /// </summary>
+        DateTime GetNetworkTime(string serverURL);
+    }
+}
