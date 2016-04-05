3b5221b0c85a7329be6bd15a615ebb18206c3583
arkeine <rytenil@gmail.com>
2016-04-05 21:39:41 +0200

    Create the interface for the game logic/rules

---
+++ fierce-galaxy/FierceGalaxyInterface/GameModule/IGame.cs
@@ -0,0 +1,28 @@
+﻿using FierceGalaxyInterface.ConnexionModule;
+using System;
+
+namespace FierceGalaxyInterface.GameModule
+{
+    public delegate void OnNodeUpdate(DateTime timestamp, int nodeID, IReadOnlyPlayer owner, int ressourcesOffset);
+    public delegate void OnSquadLeaving(DateTime timestamp, int sourceNodeID, int targetNodeID, IReadOnlyPlayer owner, int ressources);
+    public delegate void OnUpdateMana(DateTime timestamp, IReadOnlyPlayer player, int currentAmount);
+    public delegate void OnGameFinish(DateTime timestamp);
+
+    public interface IGame
+    {
+        event OnNodeUpdate NodeUpdateListener;
+        event OnSquadLeaving SquadLeavingListener;
+        event OnUpdateMana UpdateManaListener;
+        event OnGameFinish GameFinishListener;
+
+        void Move(IReadOnlyPlayer player, int sourceNodeID, int targetNodeID, int ressources);
+
+        void UsePowerDestroy(IReadOnlyPlayer player, int targetNodeID);
+
+        void UsePowerInvincibility(IReadOnlyPlayer player, int targetNodeID);
+
+        void UsePowerArmor(IReadOnlyPlayer player, int targetNodeID);
+
+        void UsePowerTeleportation(IReadOnlyPlayer player, int sourceNodeID, int targetNodeID, int ressources);
+    }
+}
