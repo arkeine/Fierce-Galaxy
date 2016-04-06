cabc304399e5eb4bf46fc882c6475c648bfd11a7
arkeine <rytenil@gmail.com>
2016-04-06 12:19:11 +0200

    Add the interface for the map manager

---
+++ fierce-galaxy/FierceGalaxyInterface/MapModule/IMapManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FierceGalaxyInterface.MapModule
+{
+    /// <summary>
+    /// The MapManager load/save the map from/to the disk
+    /// </summary>
+    public interface IMapManager
+    {
+        IList<String> MapsName { get; }
+        
+        IReadOnlyMap LoadMap(String mapName);
+
+        void SaveMap(IReadOnlyMap map);
+    }
+}
