0ca71e02b9d259d5bd46c0cc7e3f39ebb0513d61
arkeine <rytenil@gmail.com>
2016-04-12 06:44:33 +0200

    Add doc

--- fierce-galaxy/FierceGalaxyInterface/IInvalidable.cs
+++ fierce-galaxy/FierceGalaxyInterface/IInvalidable.cs
@@ -2,6 +2,14 @@
 
 namespace FierceGalaxyInterface
 {
+    /// <summary>
+    /// An invalidable object is an object "valid" 
+    /// until the invalidate method is call.
+    /// 
+    /// Invalidable object can be share with other objects 
+    /// without fear that they continu using it if no 
+    /// longuer valid.
+    /// </summary>
     public interface IInvalidable
     {
         event EventHandler OnInvalidate;
