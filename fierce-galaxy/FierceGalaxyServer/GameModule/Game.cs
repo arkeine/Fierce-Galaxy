0c83e66ec5d5774d3b6ce1f50779553e6a79fd52
arkeine <rytenil@gmail.com>
2016-04-15 16:40:58 +0200

    Begin the implementation of Game core

---
+++ fierce-galaxy/FierceGalaxyServer/GameModule/Game.cs
@@ -0,0 +1,76 @@
+﻿using FierceGalaxyInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FierceGalaxyServer
+{
+    public class Game : IGame
+    {
+        //======================================================
+        // Field
+        //======================================================
+        
+        private IReadOnlyMap map;
+        private INetworkTime ntp;
+        private IDictionary<IReadOnlyNode, Int32> dicNodeValueOffset;
+
+        //======================================================
+        // Constructor
+        //======================================================
+
+        public Game(IReadOnlyMap map, INetworkTime ntp)
+        {
+            this.map = map;
+            this.ntp = ntp;
+
+            dicNodeValueOffset = new Dictionary<IReadOnlyNode, Int32>();
+        }
+
+        //======================================================
+        // Event
+        //======================================================
+
+        public event OnGameFinish GameFinishListener;
+        public event OnNodeUpdate NodeUpdateListener;
+        public event OnSquadLeaving SquadLeavingListener;
+        public event OnUpdateMana UpdateManaListener;
+
+        //======================================================
+        // Override
+        //======================================================
+
+        public void Move(IReadOnlyPlayer player, int sourceNodeID, int targetNodeID, int ressources)
+        {
+            // Take ressources from node
+
+        }
+
+        public void UsePowerArmor(IReadOnlyPlayer player, int targetNodeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UsePowerDestroy(IReadOnlyPlayer player, int targetNodeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UsePowerInvincibility(IReadOnlyPlayer player, int targetNodeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UsePowerTeleportation(IReadOnlyPlayer player, int sourceNodeID, int targetNodeID, int ressources)
+        {
+            throw new NotImplementedException();
+        }
+
+        //======================================================
+        // Private
+        //======================================================
+        
+        
+    }
+}
