ffa629ca2b4e4103236881ea8f88ac0bc536a2ba
lukas <lukas.bitter@gmail.com>
2016-04-30 16:57:31 +0200

    - ILobby definition

---
+++ fierce-galaxy/FierceGalaxyServer/MatchingModule/ByLobby/Lobby.cs
@@ -0,0 +1,12 @@
+﻿using FierceGalaxyInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FierceGalaxyServer.MatchingModule.ByLobby
+{
+    public class Lobby : ILobby
+    {
+    }
+}
