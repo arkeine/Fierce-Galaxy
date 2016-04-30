b077a84614488eab0d22ca11dc4bc3e2a3971ec4
lukas <lukas.bitter@gmail.com>
2016-04-30 17:11:19 +0200

    Lobby implementations skeleton

--- fierce-galaxy/FierceGalaxyServer/MatchingModule/ByLobby/Lobby.cs
+++ fierce-galaxy/FierceGalaxyServer/MatchingModule/ByLobby/Lobby.cs
@@ -8,5 +8,67 @@ namespace FierceGalaxyServer.MatchingModule.ByLobby
 {
     public class Lobby : ILobby
     {
+        public IReadOnlyMap CurrentMap
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int MaxPlayers
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IReadOnlyList<IReadOnlyPlayer> PlayerList
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event EventHandler MapChange;
+        public event EventHandler PlayerJoin;
+        public event EventHandler PlayerQuit;
+
+        public void Join(IReadOnlyPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KickUser(IReadOnlyPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Leave(IReadOnlyPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPlayerColor(IPlayer player, Color c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSpawn(IReadOnlyPlayer player, IReadOnlyNode node)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
