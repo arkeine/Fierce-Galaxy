f12100f9045db2d63ff6d0abb0cadf1f98a66b12
arkeine <rytenil@gmail.com>
2016-05-01 11:23:39 +0200

    Implement a basic services for test purpose

---
+++ fierce-galaxy/FierceGalaxyService/ServicesWithLogin/FierceGalaxyLoggedIn.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FierceGalaxyService.ServicesWithLogin
+{
+    public class FierceGalaxyLoggedIn : IFierceGalaxyLoggedIn
+    {
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GenerateConnexionToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
