6f894bf2d36d5656aba3a3ee015e5ef8d6717ecf
matthieu.bandelie <matbandeli@gmail.com>
2016-05-01 18:43:16 +0200

    matthieu unity map+sphere+connector

---
+++ unityWCF/Unity/New Unity Project 1/Assets/animationSphere.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationSphere : MonoBehaviour {
+    public float shiverFactor = 0.2f;
+    private int x;
+
+    // Use this for initialization
+    void Start () {
+        x = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position += new Vector3(0.1f*Mathf.Sin(shiverFactor * x++), 0.1f * Mathf.Sin(shiverFactor * x++), 0.1f*Mathf.Sin(shiverFactor * x++)) * Time.deltaTime;
+
+        //transform.position += new Vector3(0, 1, 0) * Mathf.Sin(shiverFactor * x++) * Time.deltaTime;
+        
+        //if (x > 10000) x = 1;
+    }
+}
---
+++ unityWCF/Unity/New Unity Project 1/Assets/testSphere.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class testSphere : MonoBehaviour {
+    
+    public GameObject sphere;
+    public GameObject plan;
+    public GameObject connection;
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(plan);
+
+        Vector3 sphere1Coord = Vector3.zero;
+        Vector3 sphere2Coord = new Vector3(8,0,4);
+        Vector3 sphere3Coord = new Vector3(2, 0, -4);
+        Instantiate(sphere, sphere1Coord, Quaternion.identity);
+        Instantiate(sphere, sphere2Coord, Quaternion.identity);
+        Instantiate(sphere, sphere3Coord, Quaternion.identity);
+
+        connectSpheres(sphere1Coord, sphere2Coord);
+        connectSpheres(sphere1Coord, sphere3Coord);
+
+        initSphereBehavior();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Instantiate(sphere, new Vector3(2,2,2), Quaternion.identity);
+                //Destroy((GameObject)hit);
+
+                print(hit.transform.position);
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+            }
+        }
+    }
+
+    void connectSpheres(Vector3 sphere1Coord, Vector3 sphere2Coord)
+    {
+        Vector3 connectionDirection = sphere1Coord - sphere2Coord;
+        Vector3 connectionCenter = (sphere1Coord + sphere2Coord) / 2;
+
+        var connector = Instantiate(connection, connectionCenter, Quaternion.LookRotation(connectionDirection)) as GameObject;
+        connector.transform.localScale = new Vector3(1, 1, connectionDirection.magnitude/2);
+
+        
+    }
+
+    private void initSphereBehavior()
+    {
+        
+    }
+}
